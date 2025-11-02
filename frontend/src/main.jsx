1b442b47ca061b447e1386c47c403b9e8f2da129
DESTiNYRo <destinyrosado01@gmail.com>
2025-11-02 08:07:00 -0500

    Edit Routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,16 +6,18 @@ import './colors.css'
 import App from './App.jsx'
 import Login from './components/Login.jsx'
 import Account from './components/Account.jsx'
+import DataCollection from './components/DataCollection.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
    <BrowserRouter>
   <Routes>
-    <Route path ="/slack-cluster-finder" element = {<App/>} > </Route>
-    <Route path="/home" element={<App />} />
+    <Route path ="/slack-cluster-finder" element = {<Login/>} > </Route>
     <Route path="/" element={<Login />} />
     <Route path="/log-in" element={<Login />} />
+    <Route path="/home" element={<App />} />
     <Route path="/account" element = {<Account />} />
+    <Route path ="/new-dashboard" element = {<DataCollection/>} />
   </Routes>
 </BrowserRouter>
   </StrictMode>,
