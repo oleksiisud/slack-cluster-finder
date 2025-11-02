cfc5d807937e695d2964f924f579b3a1a6707379
DESTiNYRo <destinyrosado01@gmail.com>
2025-11-02 08:06:21 -0500

    Create DataDashboard Placeholder

---
+++ frontend/src/components/DataCollection.jsx
@@ -0,0 +1,18 @@
+import Nav from "./Nav.jsx";
+import './DataCollection.css';
+
+const DataCollection = () => {
+
+
+    return (
+        <> 
+        <Nav/>
+        <div className="DataCollection">
+        <h1>Data Collection</h1>
+        <p>Placeholder to send data to backend about channel we want a dashbaord for</p>
+        </div>
+        </>
+    );
+
+}
+export default DataCollection;
